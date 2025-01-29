be629fec14a79b5ef8694738be89c31da12505db
tanjajustus <tbakhoment@gmail.com>
2025-01-29 07:17:58 -0800

    Testin Java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,37 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] numbers = {1, 2, 3, 4, 5,6,7,8,9,10};
+int sum=0;
+
+        for(int i = 0; i < numbers.length; i++){
+                System.out.println(numbers[i]);
+
+            sum+=numbers[i];
+            System.out.println(sum);
+            System.out.println(findMax(numbers));
+
+
         }
+
+            //{
+
+
+
+
     }
+
+
+    // public static  int findMax (int[] arr)
+
+    public static int findMax(int[] arr) {
+        int max = arr[0]; // stant wert max
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
 }
